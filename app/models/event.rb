654cdd8080739fb927a054ab9b56550aaadf98e1
julian3493 <julianramosarevalo@gmail.com>
2020-12-14 16:50:23 -0500

    add validations in user and event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :date, presence: :true
+  validates :location, presence: :true
+
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
 
   has_many :atendences, class_name: :Atendence, foreign_key: :attended_event_id, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 10 }
+
   has_many :events, class_name: :Event, foreign_key: :creator_id
   has_many :atendences, class_name: :Atendence, foreign_key: :atendee_id
 
