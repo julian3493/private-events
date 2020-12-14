7c5afb60817218db6062dd157d022491485fe3db
julian3493 <julianramosarevalo@gmail.com>
2020-12-14 14:29:02 -0500

    change routes file to use only required actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :sessions
-  resources :events
+  resources :users, only: [:show, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :events, only: [:index, :show, :new, :create]
   resources :atendences
   root 'events#index'
 end
